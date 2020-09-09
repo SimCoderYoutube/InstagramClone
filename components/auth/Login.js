699a1d619572de8526d7210ff72e5a2a5fcea1f2
simcoderyoutube <simplecodervideos@gmail.com>
2020-09-09 16:27:00 +0100

    [Add] Auth System with firebase

---
+++ components/auth/Login.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native'
+
+import firebase from 'firebase'
+
+export class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        }
+
+        this.onSignUp = this.onSignUp.bind(this)
+    }
+
+    onSignUp() {
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
+
+                <Button
+                    onPress={() => this.onSignUp()}
+                    title="Sign In"
+                />
+            </View>
+        )
+    }
+}
+
+export default Login
---
+++ components/auth/Register.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native'
+
+import firebase from 'firebase'
+
+export class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+
+        this.onSignUp = this.onSignUp.bind(this)
+    }
+
+    onSignUp() {
+        const { email, password, name } = this.state;
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="name"
+                    onChangeText={(name) => this.setState({ name })}
+                />
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
+
+                <Button
+                    onPress={() => this.onSignUp()}
+                    title="Sign Up"
+                />
+            </View>
+        )
+    }
+}
+
+export default Register
